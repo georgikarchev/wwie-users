9e5fd358a98c9b742fe419e46bc843654de93b81
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-23 02:41:51 +0200

    Add spring boot app

---
+++ src/main/java/com/whatwillieat/wwie_users/WwieUsersApplication.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.wwie_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwieUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwieUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatwillieat/wwie_users/WwieUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.wwie_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwieUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
