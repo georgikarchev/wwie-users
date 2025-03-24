604c78223ed7df839f200e3fda7ccce6dd68af0f
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-24 22:15:42 +0200

    Fix User Repository to use UUID

--- src/main/java/com/whatwillieat/wwie_users/repository/UserRepository.java
+++ src/main/java/com/whatwillieat/wwie_users/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.whatwillieat.wwie_users.repository;
 import com.whatwillieat.wwie_users.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
     User findByUsername(String username);
     User findByEmail(String email);
 }
